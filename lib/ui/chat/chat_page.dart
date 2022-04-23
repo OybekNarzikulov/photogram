0327cd0d5b3349fe8fa0cae6e9ad3dd281ad3dac
oybeknarzikulov <otorinolaringologik@mail.ru>
2022-04-23 13:53:07 +0500

    full app

---
+++ lib/ui/chat/chat_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:photogram_app/routes/app_routes.dart';
+import 'package:photogram_app/theme/app_color.dart';
+import 'package:photogram_app/theme/app_constants.dart';
+import 'package:photogram_app/theme/app_text_styles.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
